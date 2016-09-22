92b17ef7a37ff97838312f432718e58ae7ae361a
Raul Cardozo <robertoraulcardozo@gmail.com>
2016-09-22 10:29:13 -0300

    20161022_10:29

--- public/components/fira/encabezado.component.js
+++ public/components/fira/encabezado.component.js
@@ -3,14 +3,13 @@ angular.module('ira.fira').component('firaEncabezado', {
     bindings: {
         registro: '<'
     },
-    controller: ['$state', '_', '$uibModal', 'infoModalService', 'firaService', function ($state, _, $uibModal, infoModalService, firaService) {
+    controller: ['$state', '_', 'sessionService', 'gobiernosLocalesService', function ($state, _, sessionService, gobiernosLocalesService) {
         var $ctrl = this;
-        $ctrl.$onInit = () =>  firaService.fetch().then(registros => {
-            $ctrl.registros = registros;
-            firaService.fetchVariables().then(variables => $ctrl.variables = variables);
+        $ctrl.$onInit = () =>  sessionService.getCurrent().then(user => {
+            $ctrl.cod = user.codGL || '';
+            gobiernosLocalesService.find($ctrl.cod).then(gobierno =>
+                $ctrl.gobierno = gobierno
+            );
         });
-
-        if ($ctrl.registro) firaService.find($ctrl.registro._id).then(data => {});
-
     }]
 });
--- public/components/fira/fira.service.js
+++ public/components/fira/fira.service.js
@@ -1,7 +1,12 @@
 angular.module('ira.fira').factory('firaService', ['$http', function ($http) {
     return {
         fetch: () => $http.get('api/fira/').then(response => response.data),
+        filter: gobierno => $http.get(`api/fira/gl/${gobierno}`).then(response => response.data),
         fetchVariables: () => $http.get('api/fira/variables').then(response => response.data),
-        find: id => $http.get(`api/fira/${id}`).then(response => response.data)
+        find: id => $http.get(`api/fira/${id}`).then(response => response.data),
+        findVariable: id => $http.get(`api/fira/variable/${id}`).then(response => response.data),
+        searchVariable: (value, soloActivos) => $http.get(`api/fira/variables/search/${value}`, {
+            params: {soloActivos}
+        }).then(response => response.data)
     };
 }]);
